a951e022f62b66985dbb39960ff10371550d2ea5
Fabio Hernandez <fabiodaniel21@gmail.com>
2022-04-24 19:42:01 -0700

    Actualizando juegos

---
+++ Juego Python/Game.py
@@ -0,0 +1,44 @@
+import random
+
+TextoPreguntasTrueFalse=['Las galletas Oreo son más antiguas que las galletas de chips de chocolate',
+'Napoleon Bonaparte era extremadamente bajo',
+'El protagonista de One Piece se llama Mugiwara No Adame', 
+'Los conejos son roedores',
+'La muralla China se ve desde el epsacio',
+'Los encendedores fueron inventados antes que los cerillos',
+'Will Smith progatonizo Matrix',
+'El personaje de Goku estuvo inspirado por un mono',
+'La gema verde del infinito representa el poder',
+'El creador de Bob Esponja es un cientifico',
+'El guitarrista de Led Zeppelin se llama Slash','Krillin murio un total de 3 veces en Dragon Ball Z',
+'El protagonista de Death Note se llama Night','Cristiano Ronaldo es conocido como el Comandante']
+
+
+TextoRespuestasTrueFalse=['Verdadero, Oreo fue inventado en 1912','Falso, aunque mucha gente cree que era bajito, media 1.70 m','Falso, se llama Mugiwara no Luffy',
+'Falso, los conejos son roedores','Falso, aunque muchos creen que se ve desde el espacio, solamente alcanza a verse a una altitud de 36km',
+'Verdadero, los encendedores fueron inventados en 1823 mientras que los cerrillos en 1826','Falso, es protagonizada por Keanu Reeves',
+'Verdadero, Akira Toriyama se inspiro en un mono para crear a Goku','Falso, representa el tiempo','Verdadero, Stephen Hillenburg fue Biologo Marino',
+'Falso, se llama Jimmy Page','Verdadero, Krillin murio un total de 3 veces',
+'Falso, se llama Light','Verdadero, SIIIIIIUUUUUUU']
+
+
+print(len(TextoPreguntasTrueFalse))
+print(len(TextoRespuestasTrueFalse))
+
+def Inicio(n):
+    str(n)
+    input('Dame un nombre', n)
+    print('Esto es un juego de preguntados, porfavor del siguiente menu elige el tema')
+    print('++'*5,'--'*5)
+
+def borraConsola():
+    os.system("cls" if os.name == 'nt' else 'clear')
+
+
+#Iniciar
+
+
+n=input('Dame un nombre')
+print('Hola',n,'Esto es un juego de preguntados, porfavor del siguiente menu elige el tema')
+print('++'*5,'--'*5)
+
---
+++ Juego Python/Preguntas.py
@@ -0,0 +1,25 @@
+TextoPreguntasTrueFalse=['Las galletas Oreo son más antiguas que las galletas de chips de chocolate',
+'Napoleon Bonaparte era extremadamente bajo',
+'El protagonista de One Piece se llama Mugiwara No Adame', 
+'Los conejos son roedores',
+'La muralla China se ve desde el epsacio',
+'Los encendedores fueron inventados antes que los cerillos',
+'Will Smith progatonizo Matrix',
+'El personaje de Goku estuvo inspirado por un mono',
+'La gema verde del infinito representa el poder',
+'El creador de Bob Esponja es un cientifico',
+'El guitarrista de Led Zeppelin se llama Slash','Krillin murio un total de 3 veces en Dragon Ball Z',
+'El protagonista de Death Note se llama Night','Cristiano Ronaldo es conocido como el Comandante']
+
+
+TextoRespuestasTrueFalse=['Verdadero, Oreo fue inventado en 1912','Falso, aunque mucha gente cree que era bajito, media 1.70 m','Falso, se llama Mugiwara no Luffy',
+'Falso, los conejos son roedores','Falso, aunque muchos creen que se ve desde el espacio, solamente alcanza a verse a una altitud de 36km',
+'Verdadero, los encendedores fueron inventados en 1823 mientras que los cerrillos en 1826','Falso, es protagonizada por Keanu Reeves',
+'Verdadero, Akira Toriyama se inspiro en un mono para crear a Goku','Falso, representa el tiempo','Verdadero, Stephen Hillenburg fue Biologo Marino',
+'Falso, se llama Jimmy Page','Verdadero, Krillin murio un total de 3 veces',
+'Falso, se llama Light','Verdadero, SIIIIIIUUUUUUU']
+
+
+
+print(len(TextoPreguntasTrueFalse))
+print(len(TextoRespuestasTrueFalse))
